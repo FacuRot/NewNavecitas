09f30331c5ccb9859913bf65b60bc562ba34836f
Facundo Rotger <facu_rot@hotmail.com>
2018-02-01 15:05:37 -0300

    Update Bullet.cpp

--- Bullet.cpp
+++ Bullet.cpp
@@ -1,13 +1,18 @@
 #include "Bullet.h"
 
-Bullet::Bullet(float x, float y) {
+Bullet::Bullet() {
 	bulletTexture.loadFromFile("Resources/Textures/Bullet.png");
 	bulletSprite.setTexture(bulletTexture);
 	bulletSprite.rotate(180);
 
 	
+	isAlive = false;
+}
+
+void Bullet::setPosition(float x, float y){
 	posX = x;
 	posY = y;
+	
 	bulletSprite.setPosition(posX, posY);
 }
 
@@ -19,12 +24,20 @@ sf::FloatRect Bullet::getBounds(){
 	return bulletSprite.getGlobalBounds();
 }
 
+void Bullet::setLive(bool a){
+	isAlive = a;
+}
+
+bool Bullet::getLive(){
+	return isAlive;
+}
+
 void Bullet::setRotation(float degree){
 	bulletSprite.setRotation(degree);
 }
 
 void Bullet::setSpeed(){
-	speed = -1*speed;
+	speed = -500.0f;
 }
 
 void Bullet::update(float elapsed){
