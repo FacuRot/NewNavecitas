b3e89948b3a314a575b093b45e27445137eb32bc
Facundo Rotger <facu_rot@hotmail.com>
2018-02-01 14:58:06 -0300

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -14,6 +14,8 @@ Enemy::Enemy() {
 	shootTime = sf::seconds(3 + rand() % 11);
 	
 	enemySprite.setPosition(1+rand()%640,rand() % 400 - 500);
+	
+	isAlive = true;
 }
 
 bool Enemy::fireEvent(float elapsed){
@@ -46,10 +48,24 @@ void Enemy::update (float elapsed) {
 	}
 }
 
+void Enemy::setLive(bool a){
+	isAlive = a;
+}
+
+bool Enemy::getLive(){
+	return isAlive;
+}
+
 void Enemy::pause(){
 	speed = 0;
+}
+
+void Enemy::resetPosition(){
+	enemySprite.setPosition(1+rand()%640,rand() % 400 - 500);
+	isAlive = true;
 }
 
 void Enemy::draw (sf::RenderWindow & w) {
 	w.draw(enemySprite);
 }
+
